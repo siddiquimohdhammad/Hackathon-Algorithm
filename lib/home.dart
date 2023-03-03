86e28a42ff63d8f73db43cbfb632e2c595d7b2a6
siddiquimohdhammad <sidhammad7292@gmail.com>
2023-03-03 12:46:23 +0530

    done

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class homeScreen extends StatefulWidget {
   const homeScreen({Key? key}) : super(key: key);
@@ -10,13 +11,34 @@ class homeScreen extends StatefulWidget {
 }
 
 class _homeScreenState extends State<homeScreen> {
+  int choiceIndex=1;
+  Map<String, double> dataMap={
+    "Rice":17,
+    "Pulses":13.2,
+    "Wheat":11,
+    "Bajra":10.14,
+    "Corn":10.12,
+  };
+
+  List<Color> colorlist=[
+    Color.fromARGB(255, 47, 239, 149),
+    Color.fromARGB(255, 250, 191, 62),
+    Color.fromARGB(255, 113, 244, 73),
+    Color.fromARGB(255, 68, 94, 241),
+    Color.fromARGB(255, 245, 91, 94),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Farmer Connect'),
       ),
-      body: Center(child: Text("data")),
+     body: PieChart(
+      dataMap: dataMap,
+      colorList: colorlist,
+      chartRadius: MediaQuery.of(context).size.width/2,
+      centerText: "Crop",
+      ),
     );
   }
 }
--- lib/splash.dart
+++ lib/splash.dart
@@ -30,15 +30,34 @@ class _splashScreenState extends State<splashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+  return Scaffold(
       body: Container(
-        child: Center(
-          child: Text(
-            'Farmer Connect',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xffbcfb69), Color(0xff26bbac)],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
           ),
         ),
-        
+        // color: Colors.green.shade300,
+        child: Center(
+          child: Container(
+            height:MediaQuery.of(context).size. height * 0.2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/farmer.png"),
+                  radius: 50,
+                ),
+                Text(
+                  'Farmer',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
