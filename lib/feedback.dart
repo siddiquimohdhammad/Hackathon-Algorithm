2137e923934b5b8431282e1fb6fb682a8c7aebbe
siddiquimohdhammad <sidhammad7292@gmail.com>
2023-03-03 17:31:15 +0530

    backend connection

--- lib/feedback.dart
+++ lib/feedback.dart
@@ -18,15 +18,14 @@ class _FeedbackFormState extends State<FeedbackForm> {
   // late String Mobnum;
 
   TextEditingController namecontroller = new TextEditingController();
-  TextEditingController studentidcontroller = new TextEditingController();
-  TextEditingController programidcontroller = new TextEditingController();
-  TextEditingController gpacontroller = new TextEditingController();
+  TextEditingController addresscontroller = new TextEditingController();
   TextEditingController mobilecontroller = new TextEditingController();
+  TextEditingController descriptioncontroller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      
        appBar: AppBar(
         // centerTitle: true,
         titleSpacing: 0,
@@ -51,111 +50,70 @@ class _FeedbackFormState extends State<FeedbackForm> {
                     focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.blue, width: 2.0)),
                   ),
-                  onChanged: (name) {
-                    // getStudentName(name);
-                    // Name=name;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: TextFormField(
-                  controller: studentidcontroller,
-                  decoration: InputDecoration(
-                    labelText: "Student ID",
-                    fillColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue, width: 2.0)),
-                  ),
-                  onChanged: (id) {
-                    // getStudentID(id);
-                    // SId=id;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: TextFormField(
-                  controller: programidcontroller,
-                  // obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Student Program ID",
-                    fillColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue, width: 2.0)),
-                  ),
-                  onChanged: (programID) {
-                    // getStudyProgramID(programID);
-                    // Programid=programID;
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  maxLength: 4,
-                  controller: gpacontroller,
-                  // obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "GPA",
-                    fillColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue, width: 2.0)),
-                  ),
-                  onChanged: (Sgpa) {
-                    // getStudentGPA(gpa);
-                    // gpa=Sgpa;
-                  },
+                  
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(18.0),
                 child: TextFormField(
                   maxLength: 10,
-                  controller: mobilecontroller,
                   keyboardType: TextInputType.number,
-                  // obscureText: true,
+                  controller: mobilecontroller,
                   decoration: InputDecoration(
-                    labelText: "Added Mobile number",
+                    
+                    labelText: "Mobile Number",
                     fillColor: Colors.white,
                     focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.blue, width: 2.0)),
                   ),
-                  onChanged: (mnum) {
-                    // getStudentGPA(gpa);
-                    // Mobnum=mnum;
-                  },
+                  
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: TextFormField(
+                  maxLines: null,
+                  controller: addresscontroller,
+                  // obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Address",
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0)),
+                  ),
+                  
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: TextFormField(
+                  maxLines: null,
+                  controller: descriptioncontroller,
+                  // obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Description",
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2.0)),
+                  ),
+                 
+                ),
+              ),
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   ElevatedButton(
                     onPressed: ()  async{
-                      // createData();
+                      
                       await StudentInfo.add({
                         'name': namecontroller.text,
-                        'Stduent Id': studentidcontroller.text,
-                        'Program Id': programidcontroller.text,
-                        'GPA': gpacontroller.text,
                         'Mobile Number': mobilecontroller.text,
-                        //   'name:':Name,
-                        //   'Student ID':SId,
-                        //   "Program ID":Programid,
-                        //   "Student GPA":gpa,
-                        //   "Mobile Number":Mobnum,
+                        'Address': addresscontroller.text,
+                        'Description': descriptioncontroller.text,
+                       
                       }).then((value) => print("added"));
 
-                      // Map<String,String> dataToSave={
-                      //   'name':namecontroller.text,
-                      //   'Stduent Id': studentidcontroller.text,
-                      //   'Program Id': programidcontroller.text,
-                      //   'GPA': gpacontroller.text,
-                      //   'Mobile Number': mobilecontroller.text,
-                      
-                      // };
-                      // FirebaseFirestore.instance.collection('StudentInfo').add(dataToSave);
                     },
                     child: const Text('Create'),
                   ),
